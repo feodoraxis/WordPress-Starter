4448659378f61209a2bb78156dacbcee57151b5b
Andrei <a.smorodin39@yandex.ru>
2020-03-09 19:29:36 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="theme-color" content="#000">
     <?php wp_head(); ?>
 </head>
 <body>
