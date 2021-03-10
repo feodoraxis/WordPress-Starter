be901da7fb0b512a1e888e22ae09c72cad27bbde
Andrei <a.smorodin39@yandex.ru>
2021-03-10 14:31:23 +0200

    Update template-enqueue.php

--- inc/template-enqueue.php
+++ inc/template-enqueue.php
@@ -2,15 +2,50 @@
 if ( !defined('ABSPATH') ) die();
 
 add_action( 'wp_enqueue_scripts', function () {
-    wp_enqueue_style( 'all_libs-css', get_template_directory_uri() . '/assets/css/all_libs.min.css', Array(),     '1.0', false );
+    //wp_enqueue_style( 'all_libs-css', get_template_directory_uri() . '/assets/css/all_libs.min.css', Array(),     '1.0', false );
     
-    wp_enqueue_style( 'main-css',  get_template_directory_uri() . '/assets/css/main.css',  Array('all_libs-css'), '1.0', false );
-    wp_enqueue_style( 'media-css', get_template_directory_uri() . '/assets/css/media.css', Array('main-css'),     '1.0', false );
+    //wp_enqueue_style( 'main-css',  get_template_directory_uri() . '/assets/css/main.css',  Array('all_libs-css'), '1.0', false );
+    //wp_enqueue_style( 'media-css', get_template_directory_uri() . '/assets/css/media.css', Array('main-css'),     '1.0', false );
+    estyle([
+        Array(
+            'name' => 'all_libs',
+            'dir' => '/assets/css/all_libs.min.css',
+            'no_in_theme' => false,
+            'in_footer' => false,
+        ),
+        Array(
+            'name' => 'main',
+            'dir' => '/assets/css/main.css',
+            'no_in_theme' => false,
+            'in_footer' => false,
+        ),
+        Array(
+            'name' => 'media',
+            'dir' => '/assets/css/media.css',
+            'no_in_theme' => false,
+            'in_footer' => false,
+        ),
+    ]);
 });
 
 add_action( 'wp_enqueue_scripts', function () {
     wp_deregister_script( 'jquery' );
-    wp_enqueue_script( 'libs-js', get_template_directory_uri() . '/assets/js/libs.min.js', Array(),          '1.0', true );
     
-    wp_enqueue_script( 'common-js', get_template_directory_uri() . '/assets/js/common.js', Array('libs-js'), '1.0', true );
+    escript([
+        Array(
+            'name' => 'libs',
+            'dir' => '/assets/js/libs.min.css',
+            'no_in_theme' => false,
+            'in_footer' => true,
+        ),
+        Array(
+            'name' => 'common',
+            'dir' => '/assets/js/common.css',
+            'no_in_theme' => false,
+            'in_footer' => true,
+        ),
+    ]);
+    //wp_enqueue_script( 'libs-js', get_template_directory_uri() . '/assets/js/libs.min.js', Array(),          '1.0', true );
+    
+    //wp_enqueue_script( 'common-js', get_template_directory_uri() . '/assets/js/common.js', Array('libs-js'), '1.0', true );
 });
